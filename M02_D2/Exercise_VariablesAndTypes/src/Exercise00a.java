1cf9dd99b33060be8607a657300e8d51f97ab797
SultanAdil <sultanadil21@gmail.com>
2022-02-08 10:45:50 -0500

    M02_D2 exercise added
    Variables and Types exercise

---
+++ M02_D2/Exercise_VariablesAndTypes/src/Exercise00a.java
@@ -0,0 +1,14 @@
+public class Exercise00a {
+    public static void main(String [] args) {
+        // 1. Add a single line comment below. Hint: this instruction is a single line comment.
+        // Hello world... I guess...
+
+        /* 2. Add a multi-line comment below.
+        Hint: this instruction is a multi-line comment.
+         */
+        /*
+        * Insert random lines comment here*
+        * I can't think of anything in the morning.
+         */
+    }
+}
